8624986a197afb63f0477a896b8cbf7c9ebf8879
snaka <snaka.gml@gmail.com>
2018-11-09 07:49:53 +0900

    ハンドラの処理をpakageに委譲

---
+++ lambda/custom/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  skillName: 'Turing Complete FM プレイヤー',
+  skillNameForSpeech: 'チューリングコンプリートエフエムプレイヤー',
+  feedUrl: 'https://feeds.turingcomplete.fm/tcfm',
+  tableName: 'alexa-skill-podcasts-player',
+  podcastId: 'turingcomplete.fm',
+  podcastName: 'Turing Complete FM',
+  podcastNameForSpeech: 'チューリングコンプリートエフエム',
+  maxEpisodeCount: 100
+}
